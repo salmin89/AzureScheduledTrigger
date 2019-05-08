5c0ecbdf55368897167da4f50d20dd18e1b0c748
Salmin Skenderovic <salmin.skenderovic@consid.se>
2019-05-08 16:30:59 +0200

    Essential code implemeted

---
+++ Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureScheduledTrigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The code provided will print ‘Hello World’ to the console.
+            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
+
+            //Console.WriteLine("Hello World!");
+
+            var s = new MySeleniumTests();
+
+            s.SetupTest();
+            s.TheBingSearchTest();
+
+            Console.ReadKey();
+
+            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        }
+
+
+    }
+}
